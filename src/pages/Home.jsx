7f5361bff5265d8e7fa6c73234cd8a0d947a11cf
hanyuone <hanyuan.work@gmail.com>
2021-04-05 01:52:37 +1000

    Added favicons, transferred Michael's code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,45 +7,42 @@ import db from '../Scripts/db';
 export default class Home extends React.Component {
   componentDidMount() {
     $("#scheduler").click(function () {
-      $("#ok").html("Clicking that button would redirect to the <b>schedule</b> page.")
-      $("#editor").fadeOut(0);
-      $("#ask").fadeOut(0);
-    })
-    $("#tips").click(function () {
-      $("#ok").html("Clicking that button would redirect to the <b>tips</b> page.")
-      $("#editor").fadeOut(0);
-      $("#ask").fadeOut(0);
-    })
-    console.log(localStorage.getItem("on"));
-    if (localStorage.getItem("on") == "true") {
-      $("#editor").fadeOut(0);
-      $("#ok").html("Welcome back!");
-      $("#main").append('<button type="button" class="btn btn-primary btn-lg px-4 me-sm-3" id="cooko" style="border-radius: 25px; color: rgb(227, 99, 91); border: 3px solid rgb(227, 99, 91); background-color: rgb(182, 227, 174);"><b>Make me a first time user again (for debugging purposes only)</b></button>')
-      $("#cooko").click(function () {
-        localStorage.setItem("on", "false");
-        $("#ok").html("Success. Refresh the page to reach the first time user landing page.")
-      })
-      $("#ask").fadeOut(0);
-    }
+      window.location.href = "/scheduler";
+    });
 
-    var faded = false;
-    $("#schedule").click(function () {
-      localStorage.setItem("on", "true");
-      $("#ok").html("Now, the website would create the schedule. This button also sets the firstTimeUser cookie to false.")
+    if (localStorage.getItem("on") != null) {
       $("#editor").fadeOut(0);
+      let their_name = localStorage.getItem("on");
+      $("#ok").html("Welcome back, " + their_name + ".");
+      $("#schedule").fadeOut(0);
       $("#ask").fadeOut(0);
-    })
-    $("#schedule").fadeOut(0);
-    document.getElementById("editor").addEventListener("input", function () {
-      if (!faded) {
-        faded = true;
-        $("#ask").fadeOut(200, "swing", function () {
-          document.getElementById("ask").innerHTML = ", would you like to create a schedule?";
-          $("#ask").fadeIn(200);
-        });
-        $("#schedule").fadeIn(900, "swing");
-      }
-    }, false);
+      $("#main").append(`<button type="button" class="btn btn-primary btn-lg px-4 me-sm-3" onclick="window.location.href='/scheduler'" style="border-radius: 25px; color: rgb(227, 99, 91); border: 3px solid rgb(227, 99, 91); background-color: rgb(182, 227, 174);"><b>View my schedule</b></button>`);
+    } else {
+      $("#scheduler").fadeOut(0);
+      var faded = false;
+      $("#schedule").click(function () {
+        let their_name = $("#editor").html();
+        localStorage.setItem("on", their_name.substring(3, their_name.length - 4));
+        their_name = localStorage.getItem("on");
+        $("#ok").html("Thanks, " + their_name + ".")
+        $("#editor").fadeOut(0);
+        $("#ask").fadeOut(0);
+        $("#schedule").fadeOut(0);
+        $("#main").append(`<button type="button" class="btn btn-primary btn-lg px-4 me-sm-3" onclick="window.location.href='/scheduler'" style="border-radius: 25px; color: rgb(227, 99, 91); border: 3px solid rgb(227, 99, 91); background-color: rgb(182, 227, 174);"><b>Start creating my schedule</b></button>`);
+      })
+      $("#schedule").fadeOut(0);
+      document.getElementById("editor").addEventListener("input", function () {
+
+        if (!faded) {
+          faded = true;
+          $("#ask").fadeOut(200, "swing", function () {
+            document.getElementById("ask").innerHTML = ", would you like to create a schedule?";
+            $("#ask").fadeIn(200);
+          });
+          $("#schedule").fadeIn(900, "swing");
+        }
+      }, false);
+    }
   }
 
   render() {
@@ -53,19 +50,18 @@ export default class Home extends React.Component {
 
     return (
       <div>
-        <div className="px-4 py-5 my-5 text-center">
-          <h1 className="display-5 fw-bold">Hello </h1>
-          <h1 class="display-5 fw-bold" id="editor" contenteditable="true" style={{outline: "none"}}>stranger</h1>
-          <h1 className="display-5 fw-bold">, what is your name?</h1>
-          <div className="col-lg-6 mx-auto">
-            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-              <button type="button" className="btn btn-primary btn-lg px-4 me-sm-3"
-                style={{
-                  borderRadius: "25px",
-                  color: "rgb(227, 99, 91)",
-                  border: "3px solid rgb(227, 99, 91)",
-                  backgroundColor: "rgb(182, 227, 174)"
-                }}>
+        <div class="px-4 py-5 my-5 text-center" id="main">
+          <h1 class="display-5 fw-bold" id="ok">Hello </h1>
+          <h1 class="display-5 fw-bold" id="editor" contenteditable="true" style={{outline: "none"}}><u>stranger</u></h1>
+          <h1 class="display-5 fw-bold" id="ask">, what is your name?</h1>
+          <div class="col-lg-6 mx-auto">
+            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center" style={{paddingTop: "20px"}}>
+              <button type="button" class="btn btn-primary btn-lg px-4 me-sm-3" id="schedule" style={{
+                borderRadius: "25px",
+                color: "rgb(227, 99, 91)",
+                border: "3px solid rgb(227, 99, 91)",
+                backgroundColor: "rgb(182, 227, 174)"
+              }}>
                 <b>Create Schedule</b>
               </button>
             </div>
@@ -73,11 +69,11 @@ export default class Home extends React.Component {
         </div>
 
         <div id="foo">
-          <img className="d-block mx-auto mb-4" src="icon.png" alt="" width="300" height="80" />
+          <img class="d-block mx-auto mb-4" src={window.location.origin + "/actual.svg"} alt="" width="200" height="200" />
         </div>
 
         <div id="another">
-          <h1 className="display-5 fw-bold" style={{ color: "red" }}>
+          <h1 class="display-5 fw-bold" style={{color: "red"}}>
             {current}
           </h1>
         </div>
